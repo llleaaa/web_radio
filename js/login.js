5119dae6184cc464c398b48a5b5941770c059d63
llleaaa <54569278+llleaaa@users.noreply.github.com>
2019-10-03 16:38:25 +0800

    login

---
+++ js/login.js
@@ -0,0 +1,22 @@
+/*var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+console.log(x);*/
+function register() {
+    var x = document.getElementById("login");
+    var y = document.getElementById("register");
+    var z = document.getElementById("btn");
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+
+}
+
+function login() {
+    var x = document.getElementById("login");
+    var y = document.getElementById("register");
+    var z = document.getElementById("btn");
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0";
+}
